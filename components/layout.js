c25d14c739772b4b2c1433eed377cdd635b1a0a7
null <tunxedge@gmail.com>
2021-08-14 00:16:44 +0700

    try ant design

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,28 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
+import styles from './layout.module.css'
 import Link from 'next/link'
 import { Menu, Breadcrumb } from 'antd'
 import profilePic from '../public/images/kittinan.png'
+import { useRouter } from 'next/router';
+
+import { Layout as AntLayout } from 'antd';
+
+import { React, useState, useEffect } from "react";
+
+//const useEnhancedEffect = typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+
+function useIsClient() {
+  const [isClient, setIsClient] = React.useState(false);
+  // The following effect will be ignored on server, 
+  // but run on the browser to set the flag true
+  useEffect(() => setIsClient(true), []);
+  return isClient
+}
+
+
+
+const { Header, Footer, Sider, Content } = AntLayout;
 
 import 'antd/dist/antd.css';
 
@@ -11,10 +30,14 @@ const name = 'Kittinan'
 export const siteTitle = 'Kittinan'
 
 export default function Layout({ children, home }) {
+
+
+
   return (
     <div>
-      <div className={styles.container}>
-        <Head>
+      <AntLayout className="layout">
+        <Header>
+
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="72x72" href="/android-icon-72x72.png"></link>
 
@@ -22,53 +45,80 @@ export default function Layout({ children, home }) {
 
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
-        </Head>
 
-        <header className={styles.header}>
 
-          {home ? (
-            <>
-              <img
-                priority
-                src="/images/kittinan.png"
-                className={utilStyles.borderCircle}
-                height={144}
-                width={144}
-                alt={name}
-              />
-              <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            </>
-          ) : (
-            <>
-              <Link href="/">
-                <a>
-                  <img
-                    priority
-                    src="/images/kittinan.png"
-                    className={utilStyles.borderCircle}
-                    height={108}
-                    width={108}
-                    alt={name}
-                  />
-                </a>
-              </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/">
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h2>
-            </>
+          <div className="logo" />
+
+          {useIsClient && (<Menu theme="dark" mode="horizontal">
+
+            <Menu.Item key="0">
+              <span className="nav-text">Home</span>
+            </Menu.Item>
+
+          </Menu>
           )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-          <div className={styles.backToHome}>
-            <Link href="/">
-              <a>← Back to home</a>
-            </Link>
+
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            {!home && (
+              <Breadcrumb.Item>Post</Breadcrumb.Item>
+            )}
+
+          </Breadcrumb>
+          <div className="site-layout-content">
+
+            {home ? (
+              <>
+                <img
+                  priority
+                  src="/images/kittinan.png"
+                  className={utilStyles.borderCircle}
+                  height={144}
+                  width={144}
+                  alt={name}
+                />
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
+              </>
+            ) : (
+              <>
+                <Link href="/">
+                  <a>
+                    <img
+                      priority
+                      src="/images/kittinan.png"
+                      className={utilStyles.borderCircle}
+                      height={108}
+                      width={108}
+                      alt={name}
+                    />
+                  </a>
+                </Link>
+                <h2 className={utilStyles.headingLg}>
+                  <Link href="/">
+                    <a className={utilStyles.colorInherit}>{name}</a>
+                  </Link>
+                </h2>
+              </>
+            )}
+
+            <main>{children}</main>
+
+
+            {!home && (
+              <div className={styles.backToHome}>
+                <Link href="/">
+                  <a>← Back to home</a>
+                </Link>
+              </div>
+            )}
+
           </div>
-        )}
-      </div>
-    </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </AntLayout>
+
+    </div >
   )
 }
